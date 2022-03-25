d340209b931f638037c4731f90b5871601bf2a4a
Tibe Desktop <tibe.de.clercq@gmail.com>
2022-03-25 20:44:43 +0100

    Add memory game V2
    change test scene to use memory game V2

--- Assets/Scripts/Games/Memory_Game/MemoryGame.cs
+++ Assets/Scripts/Games/Memory_Game/MemoryGame.cs
@@ -7,6 +7,7 @@ public class MemoryGame : MonoBehaviour
     #region Private Properties
     private List<PlayingCard> _cardsInGame;
     private List<PlayingCard> _selectedCards;
+    private Vector3 _nextPosition;
     #endregion
 
     #region Public Properties
@@ -25,26 +26,7 @@ public class MemoryGame : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(_cardsInGame.Count > 0)
-        {
-            if (Random.Range(0, 100) > 60)
-            {
-                int index = Random.Range(0, _cardsInGame.Count);
-                if (_cardsInGame[index].Selected == false)
-                {
-                    _cardsInGame[index].Selected = true;
-                }
-            }
-
-            for (int i = 0; i < _cardsInGame.Count; i++)
-            {
-                if (_cardsInGame[i].Selected && !_selectedCards.Contains(_cardsInGame[i]))
-                {
-                    Debug.Log("Card selected");
-                    this.SelectCard(_cardsInGame[i]);
-                }
-            }
-        }
+        this.SelectRandomCard();
     }
 
     private void GeneratePairs(int amount)
@@ -77,11 +59,41 @@ public class MemoryGame : MonoBehaviour
 
     private void SpawnCards()
     {
+        this._nextPosition = new Vector3(this.gameObject.transform.localPosition.x, this.gameObject.transform.localPosition.y + 0.5f, this.gameObject.transform.localPosition.z);
+
         for (int i = 0; i < _cardsInGame.Count; i++)
         {
-            PlayingCard clone = Instantiate(_cardsInGame[i], transform.position, transform.rotation);
+            PlayingCard clone = Instantiate(_cardsInGame[i], this._nextPosition, transform.rotation);
+            clone.transform.parent = this.gameObject.transform;
 
             _cardsInGame[i] = clone;
+
+            this._nextPosition = new Vector3(this._nextPosition.x + 0.25f, this._nextPosition.y, this._nextPosition.z);
+        }
+    }
+
+    private void SelectRandomCard()
+    {
+        if (_cardsInGame.Count > 0)
+        {
+            if (Random.Range(0, 1000) > 995)
+            {
+                int index = Random.Range(0, _cardsInGame.Count);
+                if (_cardsInGame[index].Selected == false)
+                {
+                    _cardsInGame[index].Selected = true;
+                    this.SelectCard(_cardsInGame[index]);
+                }
+            }
+
+            for (int i = 0; i < _cardsInGame.Count; i++)
+            {
+                if (_cardsInGame[i].Selected && !_selectedCards.Contains(_cardsInGame[i]))
+                {
+                    Debug.Log("Card selected");
+                    this.SelectCard(_cardsInGame[i]);
+                }
+            }
         }
     }
 
--- Assets/Scripts/Games/Memory_Game/PlayingCard.cs
+++ Assets/Scripts/Games/Memory_Game/PlayingCard.cs
@@ -14,6 +14,13 @@ public enum PlayingCardSuit
 
 public class PlayingCard : MonoBehaviour
 {
+    #region Private methods
+    private Quaternion _faceUp;
+    private Quaternion _faceDown;
+    private Quaternion _destination;
+    private float _rotationSpeed;
+    private bool _isFlipping;
+    #endregion
     #region Public properties
     public int Number;
     public PlayingCardSuit Suit;
@@ -21,10 +28,45 @@ public class PlayingCard : MonoBehaviour
     public bool Selected;
     #endregion
 
-    #region Private Methods
+    #region Private methods
     private void Start()
     {
+        this._faceDown = this.transform.localRotation;
+        this._faceUp = new Quaternion(0, 0, 1f, 0);
+        this._rotationSpeed = 180f;
+        this._isFlipping = false;
+
         this.Selected = false;
+
+        this.transform.rotation = _faceUp;
     }
+
+    private void FixedUpdate()
+    {
+        if (this.Selected)
+        {
+            this._destination = _faceDown;
+        }
+        else
+        {
+            this._destination = _faceUp;
+        }
+        
+        this.transform.localRotation = Quaternion.RotateTowards(this.transform.localRotation, this._destination, Time.deltaTime * this._rotationSpeed);
+
+
+        //if (this._isFlipping)
+        //{
+            
+        //    if (this.transform.rotation == this._faceDown)
+        //    {
+        //        Debug.Log("Stopped flipping");
+        //        this._isFlipping = false;
+        //    }
+        //}
+    }
+    #endregion
+
+    #region Public methods
     #endregion
 }
