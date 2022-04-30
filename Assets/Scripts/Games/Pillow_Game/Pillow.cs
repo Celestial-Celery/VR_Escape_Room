e67e46636860eaec77ffc6284612abd770909917
Evert Bekaert <bekaertevert@gmail.com>
2022-04-30 12:52:00 +0200

    Add fade out when selecting pillow with hands

--- Assets/Scripts/Games/Pillow_Game/Pillow.cs
+++ Assets/Scripts/Games/Pillow_Game/Pillow.cs
@@ -4,22 +4,51 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class Pillow : MonoBehaviour
 {
     public bool HasKey;
-
     public Couch Couch;
+
+    private bool fadeOut = false;
+    private float opacity = 1;
 
     void Awake()
     {
         HasKey = false;
     }
+
+    private void Update()
+    {
+        if (fadeOut && opacity >= 0f)
+        {
+            opacity -= 1.5f * Time.deltaTime;
+            this.gameObject.GetComponent<Renderer>().material.SetFloat("_Opacity", opacity);
+        }
+        if (opacity < 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
     public void Release()
     {
-        this.gameObject.GetComponent<Renderer>().material.SetFloat("_Opacity", 1f);
-        this.gameObject.transform.parent = this.Couch.gameObject.transform;
+        if (!HandManager.handsActive)
+        {
+            this.gameObject.GetComponent<Renderer>().material.SetFloat("_Opacity", 1f);
+            this.gameObject.transform.parent = this.Couch.gameObject.transform;
+        }        
     }
 
     public void Select()
-    {    
-        this.gameObject.GetComponent<Renderer>().material.SetFloat("_Opacity", 0.7f);
+    {
         Couch.Select(this, GameObject.Find("Key_Couch").GetComponent<Key>(), this.Couch);
+
+        if (HandManager.handsActive)
+        {
+            fadeOut = true;
+        }
+        else
+        {
+            this.gameObject.GetComponent<Renderer>().material.SetFloat("_Opacity", 0.7f);
+        }
     }
+
+
 }
